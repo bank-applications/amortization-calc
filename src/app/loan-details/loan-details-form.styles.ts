de2be49cf8436996c46252a8769feb0f572222e4
Praneeth <apraneeth3@gmail.com>
2025-05-05 12:42:27 +0530

    Added form to enter loan details.

---
+++ src/app/loan-details/loan-details-form.styles.ts
@@ -0,0 +1,15 @@
+export const formElementStyles = {
+  root: {
+    background: '#ffffff29',
+    color: '#ffffff'
+  }
+}
+
+export const submitButtonStyles = {
+  root: {
+    color: '#ffffff',
+    background: '#1e3a8a'
+  }
+}
+
+
--- src/app/loan-details/loan-details.component.ts
+++ src/app/loan-details/loan-details.component.ts
@@ -1,23 +1,45 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {FloatLabel} from "primeng/floatlabel";
+import {InputText} from "primeng/inputtext";
+import {DatePickerModule} from 'primeng/datepicker';
+import {formElementStyles, submitButtonStyles} from "./loan-details-form.styles";
+import {ButtonModule} from "primeng/button";
+import {LoanDetailsService} from "../services/loan-details.service";
+import {LoanDetails} from "../domain/loan-details-domain";
 
 @Component({
   selector: 'amort-loan-details',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    FloatLabel,
+    ButtonModule,
+    DatePickerModule,
+    InputText
   ],
   templateUrl: './loan-details.component.html',
   styleUrl: './loan-details.component.css'
 })
-export class LoanDetailsComponent {
+export class LoanDetailsComponent implements OnInit {
   formGroup = new FormGroup({
-    principal: new FormControl(0, [Validators.minLength(2), Validators.required]),
-    roi: new FormControl(0, [Validators.minLength(1), Validators.required]),
-    tenure: new FormControl(0, [Validators.minLength(1), Validators.required]),
+    principal: new FormControl(100000000, [Validators.minLength(2), Validators.required]),
+    roi: new FormControl(8.8, [Validators.minLength(1), Validators.required]),
+    tenure: new FormControl(30, [Validators.minLength(1), Validators.required]),
     startDate: new FormControl(new Date(), Validators.required)
   });
   value: string = 'Test';
+  formElementStyles = formElementStyles;
+  submitButtonStyles = submitButtonStyles;
+
+  loanDetailsService: LoanDetailsService = inject(LoanDetailsService);
+
+  ngOnInit(): void {
+  }
+
+  onSubmitDetails(): void {
+    this.loanDetailsService.loanDetails = this.formGroup.value as LoanDetails;
+  }
+
 }
