8e10932406745e8d78adbc43b8a5ec42f376183f
Praneeth <apraneeth3@gmail.com>
2025-05-05 12:41:43 +0530

    Removed unused imports.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { FilterByYearPipe } from './app/filter-by-year.pipe';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
 
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(FormsModule),
-
-  ]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
