d86750db024597cd8275b03fad7b354a8731a7c7
Praneeth <apraneeth3@gmail.com>
2025-05-14 14:49:36 +0530

    Added logic to display Amortisation Report only when data is available.

--- src/app/amortisation-dashboard/amortisation-dashboard.component.ts
+++ src/app/amortisation-dashboard/amortisation-dashboard.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoanDetailsComponent} from "../loan-details/loan-details.component";
 import {AmortisationReportComponent} from "../amortisation-report/amortisation-report.component";
+import {LoanDetailsService} from "../services/loan-details.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'amort-amortisation-dashboard',
   standalone: true,
   imports: [
     LoanDetailsComponent,
-    AmortisationReportComponent
+    AmortisationReportComponent,
+    NgIf
   ],
   templateUrl: './amortisation-dashboard.component.html',
   styleUrl: './amortisation-dashboard.component.css'
 })
-export class AmortisationDashboardComponent {
+export class AmortisationDashboardComponent implements OnInit {
+  displayReport = false;
+
+  constructor(private loanDetailsService: LoanDetailsService) {
+  }
+
+  ngOnInit() {
+    this.loanDetailsService.amortisationReport$.subscribe((res) => {
+      this.displayReport = res.length > 0;
+    })
+  }
 
 }
